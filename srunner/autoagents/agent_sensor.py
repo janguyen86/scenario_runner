639b88abcb7fe2d24d52b724f1204f28639295f4
janguyen86 <nguyen.janice86@gmail.com>
2024-02-02 10:17:35 -0800

    latest version

--- srunner/autoagents/agent_sensor.py
+++ srunner/autoagents/agent_sensor.py
@@ -5,15 +5,24 @@ import carla
 from srunner.autoagents.sensor_interface import SensorInterface
 from srunner.scenariomanager.timer import GameTime
 from srunner.tools.route_manipulation import downsample_route
-
+from agent_wrapper import * 
 from srunner.autoagents.autonomous_agent import AutonomousAgent
 
 class AgentSensor(AutonomousAgent):
+    _sensors = None 
 
-    def __init__(self, width, height): 
-        self._width = width 
-        self._height = height 
-        self._surface = None 
+    def __init__(self,
+                 vehicle,
+                 agent_config,
+                 ):
+        self.agent_id = agent_config.name
+        self.sensor_interface = SensorInterface()
+       
+    @staticmethod
+    def get_sensors():
+
+        return AgentSensor._sensors
     
-    def run_step(self, input_data, timestamp):
+    def run_step(self):
         #TODO: output agent's sensor data 
+        sensor_data = self.sensor_interface.get_data()
